2670c91fea28fbb60c7a3b05610f73b8ef9256c3
Sanara Maciel Felicio <sanara@mcity423.aquarius.cpqd.com.br>
2024-03-08 16:28:25 -0300

    refatorando readreporting services para threads

--- kafka_idempotencia_e_garantias/ecommerce/common-kafka/src/main/java/br/com/sanara/ecommerce/consumer/ConsumerService.java
+++ kafka_idempotencia_e_garantias/ecommerce/common-kafka/src/main/java/br/com/sanara/ecommerce/consumer/ConsumerService.java
@@ -3,9 +3,11 @@ package br.com.sanara.ecommerce.consumer;
 import br.com.sanara.ecommerce.Message;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.io.IOException;
+
 public interface ConsumerService<T> {
 
-    void parse(ConsumerRecord<String, Message<String>> record);
+    void parse(ConsumerRecord<String, Message<T>> record) throws IOException;
 
     String getTopic();
 
--- kafka_idempotencia_e_garantias/ecommerce/service-reading-report/src/main/java/br/com/sanara/ecommerce/ReadingReportService.java
+++ kafka_idempotencia_e_garantias/ecommerce/service-reading-report/src/main/java/br/com/sanara/ecommerce/ReadingReportService.java
@@ -1,31 +1,28 @@
 package br.com.sanara.ecommerce;
 
-import br.com.sanara.ecommerce.consumer.KafkaService;
+import br.com.sanara.ecommerce.consumer.ConsumerService;
+import br.com.sanara.ecommerce.consumer.ServiceRunner;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 /**
  * Servico para geracao de relatórios em batch
+ * refatorado para rodar com threads em paralelo
  */
-public class ReadingReportService {
+public class ReadingReportService implements ConsumerService<User> {
 
     private static final Path SOURCE = new File("src/main/resources/report.txt").toPath();
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException{
-        var reportService = new ReadingReportService();
-        try (var service = new KafkaService<>(ReadingReportService.class.getSimpleName(),
-                "ECOMMERCE_USER_GENERATE_READING_REPORT",
-                reportService::parse, Map.of())) {
-            service.run();
-        }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new ServiceRunner(ReadingReportService::new).start(5);
     }
 
-    private void parse(ConsumerRecord<String, Message<User>> record) throws IOException {
+    @Override
+    public void parse(ConsumerRecord<String, Message<User>> record) throws IOException {
         System.out.println("------------------------------------------");
         System.out.println("Processando relatório para " + record.value());
 
@@ -36,7 +33,13 @@ public class ReadingReportService {
         IO.append(target, "Criado por " + user.getUuid());
 
         System.out.println("Arquivo criado: " + target.getAbsolutePath());
-
     }
-
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_USER_GENERATE_READING_REPORT";
+    }
+    @Override
+    public String getConsumerGroup() {
+        return ReadingReportService.class.getSimpleName();
+    }
 }
